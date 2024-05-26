1a13be9de27060cc77b67c114691e7843f4fdb7a
pavlu <gavriass1@gmail.com>
2024-05-26 17:31:29 +0700

    as

--- 8Т32 лаба №4 Гаврильев Павел/laba 4_4/laba 4_4/laba 4_4.cpp
+++ 8Т32 лаба №4 Гаврильев Павел/laba 4_4/laba 4_4/laba 4_4.cpp
@@ -1,4 +1,4 @@
-﻿#include "read_file.h"
+﻿#include "readfile.h"
 #include <string>
 #include <iostream>
 
@@ -7,4 +7,5 @@ int main()
     setlocale(LC_ALL, "ru");
     std::string stroka;
     read_file(stroka,"stdfile.txt");
+    std::cout<<stroka;
 }
