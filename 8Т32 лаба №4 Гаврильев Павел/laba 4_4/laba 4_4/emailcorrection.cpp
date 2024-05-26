b570948cc9748270016cf33b35743f76160f1b93
pavlu <gavriass1@gmail.com>
2024-05-26 17:50:35 +0700

    jk

---
+++ 8Т32 лаба №4 Гаврильев Павел/laba 4_4/laba 4_4/emailcorrection.cpp
@@ -0,0 +1,60 @@
+#include "emailcorrection.h"
+
+void emailcorrection(std::string stroka)
+{
+    //33 35-39 42-43 45 47 61 63 94-96 123-126 символы 48-57 числа 65-90 97-122 буква
+    int symbol[] = { 33,35,36,37,38,39,42,43,45,47,61,63,94,95,96,123,124,125,126 };//разрешенные символы
+    bool flag1 = false;
+    bool flag2 = false;
+    bool flag3 = false;
+    int i = 0;
+    for (i = 0; i < size(stroka); i++) {//проверка локальной части
+        int sflag = false;
+        for (int j = 0; j < sizeof(symbol); j++) {
+            if (stroka[i] == symbol[j]) {
+                sflag = true;
+            }
+        }
+        if ((stroka[i] < 123 and stroka[i]>96) or (stroka[i] < 58 and stroka[i]>46) or (sflag == true) or (stroka[i] < 91 and stroka[i]>64)) {
+            flag1 = true;
+        }
+        else {
+            flag1 = false;
+
+        }
+        if (stroka[i] == 64) {
+            flag1 = true;
+            i++;
+            break;
+        }
+    }
+    for (i; i < size(stroka); i++) {//проверка домена
+        flag2 = false;
+        if ((stroka[i] < 123 and stroka[i]>96) or (stroka[i] < 58 and stroka[i]>46) or (stroka[i] < 91 and stroka[i]>64) or (stroka[i] == 45)) {
+            flag2 = true;
+        }
+        if ((stroka[i] == 46) and (size(stroka) - i > 2)) {
+            flag2 = true;
+            i++;
+            break;
+        }
+        if (flag2 == false) {
+            flag2 = false;
+            break;
+        }
+
+
+    }
+    for (i; i < size(stroka); i++) {//проверка после точки
+        flag3 = false;
+        if (((stroka[i] < 123 and stroka[i]>96) or (stroka[i] < 58 and stroka[i]>46) or (stroka[i] < 91 and stroka[i]>64))  and (flag2 == true)) {
+            flag3 = true;
+        }
+    }
+    if (flag3 == true) {//вывод проверки
+        std::cout << "Email корректен.";
+    }
+    else {
+        std::cout << "Email некорректен.";
+    }
+}
--- 8Т32 лаба №4 Гаврильев Павел/laba 4_4/laba 4_4/laba 4_4.cpp
+++ 8Т32 лаба №4 Гаврильев Павел/laba 4_4/laba 4_4/laba 4_4.cpp
@@ -1,11 +1,15 @@
 ﻿#include "readfile.h"
 #include <string>
 #include <iostream>
+#include "emailcorrection.h"
 
 int main()
 {
+    //33 35-39 42-43 45 47 61 63 94-96 123-126 символы 48-57 числа 65-90 97-122 буква
     setlocale(LC_ALL, "ru");
     std::string stroka;
-    read_file(stroka,"stdfile.txt");
-    std::cout<<stroka;
+    stroka=read_file(stroka,"stdfile.txt");//чтение строки из файла
+    emailcorrection(stroka);//проверка Email адреса
+    
+    
 }
