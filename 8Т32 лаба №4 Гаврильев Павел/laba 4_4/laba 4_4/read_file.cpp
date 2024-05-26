86afd22e87e61e256aa1e9aa395ad99497169094
pavlu <gavriass1@gmail.com>
2024-05-26 17:10:59 +0700

    4.4

---
+++ 8Т32 лаба №4 Гаврильев Павел/laba 4_4/laba 4_4/read_file.cpp
@@ -0,0 +1,13 @@
+#include "read_file.h"
+
+void read_file(char* stroka, std::string file_name) {
+    std::ifstream fin(file_name);
+    if (fin.is_open()) {
+        std::cout << "Файл открыт успешно." << std::endl;
+    }
+    else {
+        std::cout << "Ошибка открытия файла.";
+    }
+    fin.getline(stroka, 50);
+    fin.close();
+}
