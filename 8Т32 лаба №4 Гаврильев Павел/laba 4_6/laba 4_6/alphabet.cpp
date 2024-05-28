ed83aa2c2c4592f04aec895e9468ce5899e36996
pavlu <gavriass1@gmail.com>
2024-05-28 15:18:17 +0700

    d

---
+++ 8Т32 лаба №4 Гаврильев Павел/laba 4_6/laba 4_6/alphabet.cpp
@@ -0,0 +1,21 @@
+#include "alphabet.h"
+
+void alphabet(std::string stroka)
+{
+    bool flag;
+    for (int i = 0; i < size(stroka); i++) {
+        flag = false;
+        if ((stroka[i] == i + 97) or (stroka[i] == i + 65)) {
+            flag = true;
+        }
+        if (flag == false) {
+            break;
+        }
+    }
+    if (flag) {
+        std::cout << "yes";
+    }
+    else {
+        std::cout << "no";
+    }
+}
--- 8Т32 лаба №4 Гаврильев Павел/laba 4_6/laba 4_6/laba 4_6.cpp
+++ 8Т32 лаба №4 Гаврильев Павел/laba 4_6/laba 4_6/laba 4_6.cpp
@@ -1,9 +1,13 @@
-﻿
-
+﻿#include "readfile.h"
+#include "alphabet.h"
 #include <iostream>
+#include <string>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    setlocale(LC_ALL, "ru");
+    std::string stroka;
+    stroka = read_file(stroka, "stdfile.txt");
+    alphabet(stroka);
 }
 
