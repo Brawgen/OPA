690deea7656b6febbdde1705ec8396646f934f0f
pavlu <gavriass1@gmail.com>
2024-05-31 20:49:19 +0700

    4,1

--- 8Т32 лаба №4 Гаврильев Павел/laba 4_1/laba 4_1/read_file.cpp
+++ 8Т32 лаба №4 Гаврильев Павел/laba 4_1/laba 4_1/read_file.cpp
@@ -1,13 +1,15 @@
 #include "read_file.h"
+#include "filesize.h"
 
 void read_file(char* stroka, std::string file_name) {
     std::ifstream fin(file_name);
     if (fin.is_open()) {
         std::cout << "Файл открыт успешно." << std::endl;
+        fin.getline(stroka, filesize("stdfile.txt"));
     }
     else {
         std::cout << "Ошибка открытия файла.";
+        exit(0);
     }
-    fin.getline(stroka, 50);
     fin.close();
 }
