313b507b8c45cc259239bf2250ac180c9f5b6f6d
Кафедра АИКС <pavlushagav2005@gmail.com>
2024-06-05 09:59:48 +0700

    5

---
+++ 8Т32 лаба №5 Гаврильев Павел/laba 5/laba 5/laba 5.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <chrono>
+#include <thread>
+
+
+using namespace std; 
+
+int main()
+{
+	cout << "___|" << "**********\n";
+	cout << "\n___|" << "*****";
+	
+	std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+	cout << "\033[2J\033[1;1H";
+	cout << "___|" << "*********\n";
+	cout << "\n___|" << "****";
+	std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+	cout << "\033[2J\033[1;1H";
+	cout << "___|" << "********\n";
+	cout << "\n___|" << "***";
+	
+}
