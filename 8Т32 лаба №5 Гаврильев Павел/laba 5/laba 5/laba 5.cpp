cd524724cf0d44011947e90a0a6faaffeb2f3517
pavlu <gavriass1@gmail.com>
2024-06-06 20:40:26 +0700

    sd

--- 8Т32 лаба №5 Гаврильев Павел/laba 5/laba 5/laba 5.cpp
+++ 8Т32 лаба №5 Гаврильев Павел/laba 5/laba 5/laba 5.cpp
@@ -1,7 +1,7 @@
 ﻿#include <iostream>
 #include <chrono>
 #include <thread>
-
+#include <string>
 
 using namespace std; 
 
@@ -9,14 +9,24 @@ int main()
 {
 	cout << "___|" << "**********\n";
 	cout << "\n___|" << "*****";
+	cout << "\x1B[2J\x1B[H";
+	int a = 1;
+	int i = 1;
+	std::string stroka[]{"*"};
+	while (true) {
+		i++;
+		std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+		cout << "\x1B[2J\x1B[H";
+		cout << "___|";
+		for (int j = 1; j <= i; j++) {
+			cout << "*";
+		}
+		cout << "\n___|";
+		for (int j = 1; j <= i; j++) {
+			cout << "*";
+		}
+		cout<<"\n";
+	}
 	
-	std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-	cout << "\033[2J\033[1;1H";
-	cout << "___|" << "*********\n";
-	cout << "\n___|" << "****";
-	std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-	cout << "\033[2J\033[1;1H";
-	cout << "___|" << "********\n";
-	cout << "\n___|" << "***";
 	
 }
