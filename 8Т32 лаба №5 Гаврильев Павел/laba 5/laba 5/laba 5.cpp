12da6a82ed02e21d5a48a0a3391a19be181dd33c
pavlu <gavriass1@gmail.com>
2024-06-11 19:00:16 +0700

    рабочий вариант

--- 8Т32 лаба №5 Гаврильев Павел/laba 5/laba 5/laba 5.cpp
+++ 8Т32 лаба №5 Гаврильев Павел/laba 5/laba 5/laba 5.cpp
@@ -18,7 +18,7 @@ int main()
 	std::random_device rd;//получение началоьного значения от аппаратного обеспечения
 	std::mt19937 gen(rd());//создайте объект класса engine
 	std::uniform_int_distribution<> digitdist(1, 50);//диапазон для чисел
-	int size = 5;
+	int size = digitdist(gen);
 	rear = size;
 	if (size > 0 and size < 16) {
 		k1 = true;
@@ -29,13 +29,13 @@ int main()
 	if (size > 15 and size < 31) {
 		k1 = true;
 		k2 = true;
-		size1 = size/2;
+		size2 = size/2;
 		if (size%2==0)
 		{
-			size2 = size / 2;
+			size1 = size / 2;
 		}
 		else {
-			size2 = size / 2+1;
+			size1 = size / 2+1;
 		}
 		size3 = 0;
 	}
@@ -43,14 +43,14 @@ int main()
 		k1 = true;
 		k2 = true;
 		k3 = true;
-		size1 = size / 3;
 		size2 = size / 3;
-		if (size % 3 == 0)
+		size3 = size / 3;
+		if (size % 3 != 0)
 		{
-			size3 = size / 3;
+			size1 = size / 3+1;
 		}
 		else {
-			size3 = size / 3;
+			size1 = size / 3;
 		}
 	}
 	int* mass = new int[size];
@@ -63,7 +63,7 @@ int main()
 	while (size!=0) {
 		cout << "\x1B[2J\x1B[H";
 		
-		cout << "___| ";
+		cout << "___|\n___|\n___|";
 		if (k1 == true and k2 == false and k3 == false) {
 			for (int j = 0; j < size; j++) {
 				mass1[j] = mass[j];
@@ -99,6 +99,9 @@ int main()
 				}
 				mass3[j] = mass[l];
 				l++;
+				if (l == size) {
+					break;
+				}
 			}
 		}
 	work(mass1, mass2, mass3, k1, k2, k3, size1,size2, size3);
